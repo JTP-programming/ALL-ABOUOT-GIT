b4f155eb548568b29a97167eaebe6b2aa0b4bb31
JTP-programming <cinammonseal988@gmail.com>
2022-12-26 15:23:02 +0100

    CODE

--- insertData.js
+++ insertData.js
@@ -4,7 +4,7 @@ function insertRow() {
   const [name, color, weight] = process.argv.slice(2);
   db.run(
     `INSERT INTO user (firstname, lastname, username, email, password) VALUES (?,?,?,?,?)`,
-    ["Khalid", "Muhammed", "KM123", "khalid.muhammed@outlook.com", "twenty_twelve"],
+    ["Rayan", "Muhammed", "RM231", "rayan.muhammed@outlook.com", "twenty_fifteen"],
     function (error) {
       if (error) {
         console.error(error.message);
